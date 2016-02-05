5d98b6cf4731d0904d7706e62aa5953b2968bfb6
DenDoronin <den.doronind@gmail.com>
2016-02-05 19:06:35 +0200

    Initial Commit

---
+++ HILSimpleNavigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HILSimpleNavigation
+//
+//  Created by Doronin Denis on 2/5/16.
+//  Copyright © 2016 Hillel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HILSimpleNavigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HILSimpleNavigation
+//
+//  Created by Doronin Denis on 2/5/16.
+//  Copyright © 2016 Hillel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
